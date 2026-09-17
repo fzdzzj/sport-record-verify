f36c58538460f57dbe7cae3972b3658f7dfbad32
fzdzzj <fzdzzj@users.noreply.github.com>
2026-09-17 18:50:19 +0800

    chore(web): 配置 TS 别名与文件路由

---
+++ web/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import VueRouter from 'unplugin-vue-router/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    VueRouter({
+      routesFolder: './src/pages',
+      extensions: ['.page.vue'],
+      dts: './src/typed-router.d.ts',
+    }),
+    tailwindcss(),
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    port: 5173,
+  },
+})
